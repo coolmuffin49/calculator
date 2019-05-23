4027abba16dc72ee0dae3df44d7200034eaa878e
coolmuffin49 <coolmuffin49@gmail.com>
2019-05-23 11:55:48 +0700

    Updates

---
+++ precache-manifest.83e51ee2d0790518cffa569ce20fd319.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee0e3f815eeba72b14d5979028ce48c0",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "2cc7320745bfb8d0a196",
+    "url": "/calculator/static/css/main.e9ae3604.chunk.css"
+  },
+  {
+    "revision": "83242b2b2adf52191f75",
+    "url": "/calculator/static/js/2.31ba06cb.chunk.js"
+  },
+  {
+    "revision": "2cc7320745bfb8d0a196",
+    "url": "/calculator/static/js/main.af974271.chunk.js"
+  },
+  {
+    "revision": "2fe8cbdbbea1fbf116e6",
+    "url": "/calculator/static/js/runtime~main.2539f434.js"
+  },
+  {
+    "revision": "f2c73add938958c164066ff3467cb8e9",
+    "url": "/calculator/static/media/math.f2c73add.jpg"
+  }
+]);
